c0c735a03d37d1ddf78486c8d2f7e89602a0f6f6
Albin Lagerquist <lagerquist.albin@gmail.com>
2021-06-10 12:47:03 +0200

    prettier

--- include/config.php
+++ include/config.php
@@ -1,21 +1,21 @@
 <?php
 
-// Active page variable
-$current_page = basename($_SERVER['PHP_SELF']);
-
-// Connection variables
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "filmshelf";
-
-// Set connection query
-$db_connect = new mysqli($host, $user, $password, $database);
-
-// Check connection
-if ($db_connect -> connect_errno) {
-  echo "Failed to connect to database: " . $db_connect -> connect_error;
-  exit();
-}
+  // Active page variable
+  $current_page = basename($_SERVER['PHP_SELF']);
+  
+  // Connection variables
+  $host = "localhost";
+  $user = "root";
+  $password = "";
+  $database = "filmshelf";
+  
+  // Set connection query
+  $db_connect = new mysqli($host, $user, $password, $database);
+  
+  // Check connection
+  if ($db_connect -> connect_errno) {
+    echo "Failed to connect to database: " . $db_connect -> connect_error;
+    exit();
+  }
 
 ?>
