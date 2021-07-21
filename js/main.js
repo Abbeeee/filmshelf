f1f243d822838093040a92dd35f20aced85d1662
Albin Lagerquist <lagerquist.albin@gmail.com>
2021-07-21 18:57:38 +0200

    fixed }

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ function slideShow() {
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";  
   setTimeout(slideShow, 6000); // Change image every 6 seconds
+  }
 }
 
 function goBack() {
