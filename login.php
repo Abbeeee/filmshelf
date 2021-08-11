01b67bd92dab2e4208fedbb6543c0ca4efe9fa54
Albin Lagerquist <lagerquist.albin@gmail.com>
2021-08-11 20:49:49 +0200

    login & shelf pages fixes

--- login.php
+++ login.php
@@ -20,33 +20,35 @@
     background-image: linear-gradient(to bottom, rgba(255,0,0,0), rgb(10, 10, 10) 80%),
     url('http://image.tmdb.org/t/p/original/7JENyUT8ABxcvrcijDBVpdjgCY9.jpg');
     ">
-</div>
+
     
 
-<div class="container custom-container login-container">
-  <h1 class="display-4 mb-4">Login</h1>
-  <p class="lead mb-4">This is the login section where you can login.</p>
-
-    <!-- When button is pressed, run login-config.php file -->
-    <form class="login-form" action="login-config.php" method="post">
-
-      <div class="form-group">
-        <label>Username</label>
-        <input type="text" class="form-control" name="username" placeholder="Enter your Username...">
-      </div>
-
-      <div class="form-group">
-        <label>Password</label>
-        <input type="password" class="form-control" name="password" placeholder="Enter your Password...">
-      </div>
-
-      <button class="btn btn-light mt-3" type="submit" value="Login">Login</button>
-
-      <?php if (isset($_GET['error'])) { ?>
-            <span class="error"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather-alert-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg><?php echo $_GET['error'] ?></span>
-      <?php } ?>
-
-    </form>
+  <div class="container custom-container login-container">
+    <h1 class="display-4 mb-4">Login</h1>
+    <p class="lead mb-4">This is the login section where you can login.</p>
+  
+      <!-- When button is pressed, run login-config.php file -->
+      <form class="login-form" action="login-config.php" method="post">
+  
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" name="username" placeholder="Enter your Username...">
+        </div>
+  
+        <div class="form-group">
+          <label>Password</label>
+          <input type="password" class="form-control" name="password" placeholder="Enter your Password...">
+        </div>
+  
+        <button class="btn btn-light mt-3" type="submit" value="Login">Login</button>
+  
+        <?php if (isset($_GET['error'])) { ?>
+              <span class="error"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather-alert-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg><?php echo $_GET['error'] ?></span>
+        <?php } ?>
+        
+      </form>
+        
+  </div>
 
 </div>
 
--- shelf.php
+++ shelf.php
@@ -27,67 +27,69 @@ if (isset($_SESSION['userName'])) {
     background-image: linear-gradient(to bottom, rgba(255,0,0,0), rgb(10, 10, 10) 80%),
     url('http://image.tmdb.org/t/p/original/s4wRC37TUwxtghhFBaze0qO1278.jpg');
     ">
-</div>
+
     
-<div class="container custom-container">
-  <form class="btn-container" action="logout.php">
-    <button class="btn btn-danger">Log out</button>
-  </form>
-  <h1 class="display-4 mb-4">Welcome <?php echo $_SESSION['userName']; ?></h1>
-  <p class="lead mb-4">This is the shelf section where you can view any lists and movies you have added to your shelf.</p>
+  <div class="container custom-container">
+    <form class="btn-container" action="logout.php">
+      <button class="btn btn-danger">Log out</button>
+    </form>
+    <h1 class="display-4 mb-4">Welcome <?php echo $_SESSION['userName']; ?></h1>
+    <p class="lead mb-4">This is the shelf section where you can view any lists and movies you have added to your shelf.</p>
 
-<?php
-    // Save userID from session from whatever user is currently logged in
-    $current_userID = $_SESSION['userID'];
-    // Display the lists the current user has
-		$query = "SELECT lists.*
-  		        FROM lists
-              WHERE lists.userID = $current_userID";
-
-		// Prepare query as above, and save as variable $stmt
-		$stmt = $db_connect->prepare($query);
-		// Execute query
-		$stmt->execute();
-		// Get the result of query
-		$result = $stmt->get_result();
-?>
-
-  <!-- The list table -->
-  <div class="container my-5 d-flex justify-content-center table-container">
-
-    <!-- When remove button is clicked, go into remove.php -->
-    <div class="list-group">
-      <?php
+    <?php
+        // Save userID from session from whatever user is currently logged in
+        $current_userID = $_SESSION['userID'];
         // Display the lists the current user has
-		    $query = "SELECT *
-		              FROM movies
-                  JOIN movies_lists
-                  ON movies.movieID = movies_lists.movieID
-                  JOIN lists
-                  on lists.listID = movies_lists.listID
-                  WHERE lists.userID = $current_userID";  
-		    // Prepare query as above, and save as variable $stmt
-		    $stmt = $db_connect->prepare($query);
-		    // Execute query
-		    $stmt->execute();
-		    // Get the result of query
-		    $result = $stmt->get_result();
+    		$query = "SELECT lists.*
+      		        FROM lists
+                  WHERE lists.userID = $current_userID";
+    
+    		// Prepare query as above, and save as variable $stmt
+    		$stmt = $db_connect->prepare($query);
+    		// Execute query
+    		$stmt->execute();
+    		// Get the result of query
+    		$result = $stmt->get_result();
+    ?>
 
-        foreach ($result as $item) {
-            echo   '<div>';
-            echo   '<a href="single.php?'.$item['movieID'].'" class="d-inline-block list-movie">'.$item['title'].'</a>'; 
-            echo   '<a class="list-btn" href="remove.php?'.$item['movieID'].'">Remove</a>';  
-            echo   '</div>';         
-        }
+    <!-- The list table -->
+    <div class="container my-5 d-flex justify-content-center table-container">
 
-        // free results
-				$stmt->free_result();
-   			// close statement
-   			$stmt->close();
-        // close connection
-				$db_connect->close(); 
-      ?>
+      <!-- When remove button is clicked, go into remove.php -->
+      <div class="list-group">
+        <?php
+          // Display the lists the current user has
+  		    $query = "SELECT *
+  		              FROM movies
+                    JOIN movies_lists
+                    ON movies.movieID = movies_lists.movieID
+                    JOIN lists
+                    on lists.listID = movies_lists.listID
+                    WHERE lists.userID = $current_userID";  
+  		    // Prepare query as above, and save as variable $stmt
+  		    $stmt = $db_connect->prepare($query);
+  		    // Execute query
+  		    $stmt->execute();
+  		    // Get the result of query
+  		    $result = $stmt->get_result();
+
+          foreach ($result as $item) {
+              echo   '<div>';
+              echo   '<a href="single.php?'.$item['movieID'].'" class="d-inline-block list-movie">'.$item['title'].'</a>'; 
+              echo   '<a class="list-btn" href="remove.php?'.$item['movieID'].'">Remove</a>';  
+              echo   '</div>';         
+          }
+
+          // free results
+  				$stmt->free_result();
+     			// close statement
+     			$stmt->close();
+          // close connection
+  				$db_connect->close(); 
+        ?>
+      </div>
     </div>
+
   </div>
 
 </div>
