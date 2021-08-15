6f5af3140c7a2332ee20840b20da93e1151d2873
Albin Lagerquist <lagerquist.albin@gmail.com>
2021-08-15 16:46:03 +0200

    menu toggle for mobile

--- include/header.php
+++ include/header.php
@@ -3,6 +3,7 @@
     include 'include/config.php'; 
 ?>
 
+<!--
 <nav class="navbar navbar-expand navbar-dark navbar-custom">
     <div class="container d-flex justify-content-between">
         <a class="navbar-brand mr-5" href="index.php">Filmshelf</a>
@@ -13,4 +14,39 @@
         </div>
     </div>
 </nav>
+-->
+
+<div class="navbar-custom">
+    <nav class="container navbar navbar-expand-md navbar-dark navbar-custom-2" role="navigation">
+
+        <a href="index.php" class="navbar-brand mr-auto">Filmshelf</a>
+
+        <button 
+            class="navbar-toggler" 
+            type="button" 
+            data-toggle="collapse" 
+            data-target="#toggler-menu"
+            aria-controls="toggler-menu"
+            aria-expanded="false"
+            aria-lable="Toggle navigation"    
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="toggler-menu">
+            <ul class="navbar-nav ml-auto text-center">
+                <li>
+                    <a class="nav-item nav-link <?php echo ($current_page == 'index.php') ? 'active' : 'not-active' ?>" href="index.php">Home</a>
+                </li>
+                <li>
+                    <a class="nav-item nav-link <?php echo ($current_page == 'browse.php') ? 'active' : 'not-active' ?>" href="browse.php">Browse</a>
+                </li>
+                <li>
+                    <a class="nav-item nav-link <?php echo ($current_page == 'login.php' || $current_page == 'shelf.php') ? 'active' : 'not-active' ?>" href="shelf.php">Lists</a>
+                </li>
+            </ul>
+        </div>
+
+    </nav>
+</div>
 
