bd31f3f1396f1d4f2a21193fb72fbacc9da84b4e
Albin Lagerquist <lagerquist.albin@gmail.com>
2021-08-15 17:08:42 +0200

    smol stoff

--- login.php
+++ login.php
@@ -39,12 +39,12 @@
           <label>Password</label>
           <input type="password" class="form-control" name="password" placeholder="Enter your Password...">
         </div>
-  
-        <button class="btn btn-light mt-3" type="submit" value="Login">Login</button>
-  
+
         <?php if (isset($_GET['error'])) { ?>
-              <span class="error"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather-alert-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg><?php echo $_GET['error'] ?></span>
+              <p class="error"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather-alert-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg><?php echo $_GET['error'] ?></p>
         <?php } ?>
+  
+        <button class="btn btn-secondary mt-3" type="submit" value="Login">Login</button>
         
       </form>
         
